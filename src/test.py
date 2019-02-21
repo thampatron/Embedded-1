a586e79425a420411c34689e8e72e41f317713dd
Jordi Laguarta Soler <jordi.laguarta.soler@gmail.com>
2019-02-21 13:39:56 +0000

    PALOMA_almostdone

--- src/test.py
+++ src/test.py
@@ -4,10 +4,10 @@ import time
 
 client = initSender("PalomAlert/run")
 
-send(client, None, "PalomAlert/run", qos=1)
+send(client, None, "PalomAlert/run", qos=2)
 
 time.sleep(20)
 
-send(client, None, "PalomAlert/halt", qos=1)
+send(client, None, "PalomAlert/halt", qos=2)
 
 print("The test is complete!")
