7902c0575fd4d90e0462ef6b1ca1f41b52d21c53
Jordi Laguarta Soler <jordi.laguarta.soler@gmail.com>
2019-02-21 16:39:13 +0000

    FINAL WEB DEMO

--- src/palomAlert.py
+++ src/palomAlert.py
@@ -3,23 +3,23 @@ import json
 import time
 from API_network import send, initSender
 
-
-
 client = initSender("PalomAlert/run")
 
+#read last status from status.json
 def get_status():
     with open("status.json", "r") as f:
         status_dict = json.load(f)
     return status_dict
 
+#read logged events from log.txt
 def get_log():
   with open("log.txt") as f:
     content = f.readlines()
-# you may also want to remove whitespace 
-# characters like `\n` at the end of each line
+  #to remove whitespace characters like `\n` at the end of each line
   content = [x.strip() for x in content] 
   return content
 
+#get relevant information from log to display on Events Log
 def get_logStatus(log):
   output = []
   t_output = [] 
@@ -27,14 +27,15 @@ def get_logStatus(log):
 
     event = json.loads(m_event)
     t_stamp = event["timeStamp"]
-    
+  
+    #check what changed in the event by comparing event update-time and component update-time
     if time.ctime(event["status"]["compass"]["lastUpdate"]) == t_stamp:
       if event["status"]["compass"]["isOpen"] == 'yes':
         was_open = "opened"
       else :
         was_open = "closed"
       output.append("Door was "+was_open)
-      t_output.append( t_stamp)
+      t_output.append(t_stamp)
     elif time.ctime(event["status"]["accelerometer"]["lastIntrusion"]) == t_stamp:
       output.append("Door was shaken ")
       t_output.append(t_stamp)
@@ -43,8 +44,11 @@ def get_logStatus(log):
 
 
 app = Flask(__name__)
+
+#route called when "AT HOME?" is changed to update at_home status and turn RUN/HALT the pi
 @app.route('/postmethod', methods = ['POST'])
 def get_post_javascript_data():
+    
     jsdata = request.form['javascript_data']
     if jsdata == "no":
       app.at_home = "yes"
@@ -52,64 +56,35 @@ def get_post_javascript_data():
     else:
       app.at_home="no"
       send(client, None, "PalomAlert/run", qos=2)
-
-
     your_list = get_log()
-    print(your_list[0])
-    your_list = your_list[:3]
+    your_list = list(reversed(your_list[(len(your_list)-4):]))
 
-
-    print (app.at_home)
     status = get_status()
-  
     
-    last_update = status["lastFileUpdate"]
+    last_update = time.ctime(status["lastFileUpdate"])
     temp = status["thermometer"]["temperature"]
-    print(your_list)
+   
     is_open = status["compass"]["isOpen"]
-    #time_ago = last_update - time.time
-    #print(time_ago)
+  
     log, t_log = get_logStatus(your_list)
 
-    print(app.at_home)
-    return  render_template("paloma.html", temp=temp, is_open=is_open,your_list=log,t_log = t_log , t_open = last_update, at_home=app.at_home, system_status="connected") 
+    
+    return  render_template("paloma.html", temp=temp, is_open=is_open, your_list=log, t_log = t_log , t_open = last_update, at_home=app.at_home, system_status="connected") 
 
+#home route which renders html 
 @app.route("/")
 def home():
     your_list = get_log()
     status = get_status()
-    your_list = your_list[:3]
-    last_update = status["lastFileUpdate"]
+    your_list = list(reversed(your_list[(len(your_list)-4):]))
+    last_update = time.ctime(status["lastFileUpdate"])
     temp = status["thermometer"]["temperature"]
-    print(your_list)
+    
     is_open = status["compass"]["isOpen"]
     log, t_log = get_logStatus(your_list)
-    #time_ago = last_update - time.time
-    #print(time_ago)
-   
-    print(app.at_home)
-    return render_template("paloma.html", temp=temp, is_open=is_open,your_list=log,t_log=t_log,t_open = last_update, at_home=app.at_home, system_status="connected") # last_update, sysON, #, opendoor, door-shake
+    
 
-
-def status_change():
-    new_status = get_status()
-    if new_status is status:
-        return False
-    return True
-status = get_status()
-
-#print(status)
-#starttime=time.time()
-#stat_change = status_change()
-#while True:
- # if(stat_change):
-  #    home()
-    #new_status = get_status()
-    #temp = new_status["thermometer"]["temperature"]
-    #status=get_status()
-    #return render_template("paloma.html", temp=temp)
-  #app = AppReloader(get_app)
-  #time.sleep(30.0 - ((time.time() - starttime) % 30.0))
+    return render_template("paloma.html", temp=temp, is_open=is_open,your_list=log,t_log=t_log,t_open = last_update, at_home=app.at_home, system_status="connected") 
 
 
 if __name__ == "__main__":
